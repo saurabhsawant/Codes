19af6b5849e42473d65a265385a6a1e5a6f8929a
saurabhsawant <saurabh.dilip.sawant@gmail.com>
2016-03-12 14:53:01 -0600

    Create printBoundry.java

---
+++ printBoundry.java
@@ -0,0 +1,61 @@
+
+public clas TreeNode<T> {
+    public T data;
+    public TreeNode left;
+    public TreeNode right;
+
+} 
+
+public class Solution {
+    static pubic void printBoundry(TreeNode<T> root) {
+        if(root == null) return;
+        
+        if(root.left == null && root.right == null) {
+            System.put.println(root.data.toString());
+            return;
+        } //ptherwise this will get oprinted twice by left and leaf
+            
+        
+        //print left Boundry from  top - bottom
+        printLeft(root);
+        
+        // print lower left right
+        printLeft(root);
+        
+        //right boundry for bom up
+        printLeft(root.right); //Not to print root again
+    }
+
+    static public void printLeft(TreeNode<T> root) {
+        if(root==null) return;
+        
+        if(root.left==null && root.right==null) return;
+        
+        System.put.println(root.data.toString());
+        
+        printLeft(root.left);
+    }
+
+
+    static public  void printRight(TreeNode<T> root) {
+        if(root==null) return;
+        
+        if(root.left==null && root.right == null) return;
+        
+        System.put.println(root.data.toString());
+        
+        printRight(root.right;)
+    }
+
+    static public void printLeaf() {
+        if(root==null) return;
+        
+        if(root.left==null && root.right == null)
+            System.put.println(root.data.toString());
+            
+        printLeaf(root.left);
+        printLeaf(root.right);
+    
+    }
+
+}
