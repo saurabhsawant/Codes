47507a2f7fb50ec1398e072e4eef815c387fa060
saurabhsawant <saurabh.dilip.sawant@gmail.com>
2016-03-16 19:57:06 -0500

    Create heapSort.java

---
+++ heapSort.java
@@ -0,0 +1,56 @@
+public HrapSort {
+    public static void swap(int[] a, int x , int y) {
+        if(x!=y && x >= 0 && x <=a.lengh-1 && y >=0 && y<=a.lengh -1) {
+            int temp = a[x];
+            a[x] = a[y];
+            a[y] = temp;
+        }
+    }
+    
+    
+    public static maxHeapify(int[] a, int index, int heapSized) {
+        int left = 2 * index + 1;
+        int right =  2 * index + 2;
+        
+        
+        int maxindex = index;
+        if(left<=heapSize-1 && a[left] > a[maxindex])
+            maxindex = left;
+            
+        if(right<=heapSize-1 && a[right] > a[maxindex])
+            maxindex = right;
+            
+        if(index!=maxindex) {
+            swap(a, index, maxindex);
+            maxHeapify(a, maxindex, heapSize);
+        }
+    }
+    
+    public static buildMaxHeap(int[] a) {
+        if(a==null) return null;
+        
+        if(int i=(a.length -1)/2; i>=0; i--) {
+            maxHeapify(a, i, a.length);
+        }
+     
+    }
+    
+    //bubble up in max heapifyUp,
+    // for an index, get parent p = (index -1 )/2 and then do max for recursive for index and not max index
+    
+    
+    public static void heapSort(int[] a) {
+        if(a==null) throw new Exception("");
+        
+        buildMaxHeap(a);
+        
+        int heapSize = a.lenhth;
+        for(int i = a.length-1; i>= 1 ; i++) {
+            swap(0,i);
+            heapSize --;
+            maxHeapify(a, 0, heapSize);
+        }
+        
+    }
+
+}
